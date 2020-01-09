d1b49d0ec97bff3a4b7b0a8cb52a7fba3af6e292
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-01-08 23:50:10 -0500

    Class constructors. Default / Custom

--- app/src/main/java/org/ohmstheresistance/aquarium/Aquarium.kt
+++ app/src/main/java/org/ohmstheresistance/aquarium/Aquarium.kt
@@ -1,11 +1,14 @@
 package org.ohmstheresistance.aquarium
 
 //Classes
-class Aquarium {
 
-    var length: Int = 100
-    var height: Int = 40
-    var width: Int = 25
+
+//Constructors
+
+//by passing the variables in the class constructor arguments, it creates the member properties for you and assigns the passed in or default values for
+//you
+class Aquarium(var length: Int = 100, var height: Int = 40, var width: Int = 25) {
+
 
 
      fun volumeOfWater(): Int {
@@ -14,6 +17,21 @@ class Aquarium {
 
     //The about function can also be written more concisely like:
     fun volumeOfWaterInAquarium() = length * width * height / 1000
+
+
+    var water = volumeOfWater() * 0.9
+
+
+    //in case a constructor is needed that's not the default (or may hold values that the default one does not)
+    //NOTE: You can't mix arguments from both constructors. Arguments have to match exactly with one of the two constructors
+
+    constructor(numberOfFish : Int): this(){
+
+        val water :Int = numberOfFish * 2000 //cm3
+        val tank :Double = water + water * 0.1
+        height = (tank / (length * width)).toInt()
+    }
+
 }
 
 
