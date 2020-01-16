09aba3bbce74946514d05ee98cb815e303af794a
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-01-15 21:12:13 -0500

    Kotlin annotations

---
+++ app/src/main/java/org/ohmstheresistance/aquarium/aquarium/generics/Annotations.kt
@@ -0,0 +1,63 @@
+
+//Annotations
+
+//Annotations are a means of attaching meta-data to code
+//They are read by the compiler and used to generate code, or even logic
+//Annotations are not Kotlin specific, but Kotlin offers some really useful ones.
+//They go right before the thing being annotated.
+//some can even take arguments.
+
+//NOTE: Annotations can target GETTERS and SETTERS
+
+@file:JvmName("InteropFish")
+
+class InteropFish{
+    companion object{
+        @JvmStatic fun interop(){
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/org/ohmstheresistance/aquarium/aquarium/generics/GenericAquarium.kt
+++ app/src/main/java/org/ohmstheresistance/aquarium/aquarium/generics/GenericAquarium.kt
@@ -153,6 +153,48 @@ fun genericExampleThree(){
 //All generic types are only used at compile time in kotlin. However, at runtime, all generic types are erased.
 
 
+//Reflection
+
+@ImAPlant class Plant{
+    fun trim(){}
+
+    fun fertilize(){}
+
+    @get:OnGet
+    val isGrowing: Boolean = true
+
+    @set:OnSet
+    var needsFood: Boolean = false
+}
+
+//:: represents reflection in kotlin
+
+annotation class ImAPlant
+
+@Target(AnnotationTarget.PROPERTY_GETTER)
+annotation class  OnGet
+
+
+@Target(AnnotationTarget.PROPERTY_SETTER)
+annotation class OnSet
+
+fun reflections(){
+    val classObj = Plant::class
+
+    //print all annotations
+    for (annotation in classObj.annotations){
+
+        println(annotation.annotationClass.simpleName)
+    }
+
+    //find one annotation, or null
+   // val annotated = classObj.findAnnotation<ImAPlant>
+
+//    annotated?.apply{
+//        println("Found a plant annotation!")
+//    }
+}
+
 
 
 
